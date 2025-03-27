dba11cf3cfdb5b764baae6b552aaa2688d7077e4
dan-kwiat <5648607+dan-kwiat@users.noreply.github.com>
2025-03-27 21:54:29 +0000

    Add _metatx module

---
+++ servers/_metatx/src/operations/add_metatx_requester_holder_route_contracts_contract_id_holders_.ts
@@ -0,0 +1,18 @@
+import { z } from "zod"
+
+export const toolName = `add_metatx_requester_holder_route_contracts_contract_id_holders_`
+export const toolDescription = `Add Metatx Requester Holder Route`
+export const baseUrl = `/metatx`
+export const path = `/contracts/{contract_id}/holders`
+export const method = `post`
+export const security = [
+  {
+    "key": "Authorization",
+    "value": "Bearer <mcp-env-var>OAUTH2_TOKEN</mcp-env-var>",
+    "in": "header",
+    "envVarName": "OAUTH2_TOKEN",
+    "schemeType": "oauth2"
+  }
+]
+
+export const inputParams = z.object({ "path": z.object({ "contract_id": z.string().uuid() }).optional(), "body": z.object({ "holder_id": z.string().uuid(), "holder_type": z.enum(["user","group"]).describe("An enumeration."), "permissions": z.array(z.enum(["admin","create","read","update","delete"]).describe("An enumeration.")).optional() }).optional() }).shape
