50b1c627b38c91f1bb417879856e4311be2c582f
dan-kwiat <5648607+dan-kwiat@users.noreply.github.com>
2025-03-29 20:00:55 +0000

    Add api_example_com module

---
+++ servers/api_example_com/src/constants.ts
@@ -0,0 +1,6 @@
+export const SERVER_NAME = "api_example_com"
+export const SERVER_VERSION = "0.0.1"
+export const OPERATION_FILES_RELATIVE = [
+  "./operations/gettodosv1.js",
+  "./operations/createtodov1.js"
+]
