8af22b47166e818d62885537ee4d2a079d18010b
dan-kwiat <5648607+dan-kwiat@users.noreply.github.com>
2025-03-31 09:34:00 +0100

    Add api_spotify_com_v1 module

---
+++ servers/api_spotify_com_v1/src/tools/get_new_releases.ts
@@ -0,0 +1,18 @@
+import { z } from "zod"
+
+export const toolName = `get_new_releases`
+export const toolDescription = `Get New Releases`
+export const baseUrl = `https://api.spotify.com/v1`
+export const path = `/browse/new-releases`
+export const method = `get`
+export const security = [
+  {
+    "key": "Authorization",
+    "value": "Bearer <mcp-env-var>OAUTH2_TOKEN</mcp-env-var>",
+    "in": "header",
+    "envVarName": "OAUTH2_TOKEN",
+    "schemeType": "oauth2"
+  }
+]
+
+export const inputParams = z.object({ "query": z.object({ "limit": z.number().int().gte(1).lte(50).describe("The maximum number of items to return. Default: 20. Minimum: 1. Maximum: 50.\n"), "offset": z.number().int().describe("The index of the first item to return. Default: 0 (the first item). Use with limit to get the next set of items.\n") }).optional() }).shape
---
+++ servers/api_spotify_com_v1/src/tools/save_audiobooks_user.ts
@@ -0,0 +1,18 @@
+import { z } from "zod"
+
+export const toolName = `save_audiobooks_user`
+export const toolDescription = `Save Audiobooks for Current User`
+export const baseUrl = `https://api.spotify.com/v1`
+export const path = `/me/audiobooks`
+export const method = `put`
+export const security = [
+  {
+    "key": "Authorization",
+    "value": "Bearer <mcp-env-var>OAUTH2_TOKEN</mcp-env-var>",
+    "in": "header",
+    "envVarName": "OAUTH2_TOKEN",
+    "schemeType": "oauth2"
+  }
+]
+
+export const inputParams = z.object({ "query": z.object({ "ids": z.string().describe("A comma-separated list of the [Spotify IDs](/documentation/web-api/concepts/spotify-uris-ids). For example: `ids=18yVqkdbdRvS24c0Ilj2ci,1HGw3J3NxZO1TP1BTtVhpZ`. Maximum: 50 IDs.\n") }).optional() }).shape
