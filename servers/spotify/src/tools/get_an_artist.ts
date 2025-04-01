3e94ac18f07f981ba5112ce62225b9d8fcf29c2f
dan-kwiat <5648607+dan-kwiat@users.noreply.github.com>
2025-04-01 17:21:06 +0200

    feat(spotify): test npm publish

---
+++ servers/spotify/src/tools/get_an_artist.ts
@@ -0,0 +1,28 @@
+import { z } from "zod"
+
+export const toolName = `get_an_artist`
+export const toolDescription = `Get Artist`
+export const baseUrl = `https://api.spotify.com/v1`
+export const path = `/artists/{id}`
+export const method = `get`
+export const security = [
+  {
+    "key": "Authorization",
+    "value": "Bearer <mcp-env-var>OAUTH2_TOKEN</mcp-env-var>",
+    "in": "header",
+    "envVarName": "OAUTH2_TOKEN",
+    "schemeType": "oauth2"
+  }
+]
+export const keys = {
+  "query": [],
+  "header": [],
+  "path": [
+    "id"
+  ],
+  "cookie": [],
+  "body": []
+}
+export const flatMap = {}
+
+export const inputParams = z.object({ "id": z.string().describe("The [Spotify ID](/documentation/web-api/concepts/spotify-uris-ids) of the artist.\n") }).shape
